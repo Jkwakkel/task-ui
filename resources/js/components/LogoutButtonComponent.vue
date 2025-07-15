491cf3d449e3f5cb37780d584a3aeec1f25f9154
jkwakkel <jkwakkel@xs4all.nl>
2025-07-15 15:22:24 +0200

    Feat: Remove ExampleComponent add MainButtonsComponent and add Login, Register, and Logout button components. Update app.js and welcome.blade.php to reflect these changes.

---
+++ resources/js/components/LogoutButtonComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const handleLogout = () => {
+    console.log('logout');
+};
+</script>
+
+<template>
+    <button @click="handleLogout" class="btn-main">Logout</button>
+</template>
+
+<style scoped>
+
+</style>
